62e5599ed31dba13455e531fce17f6d4eb6b70c0
Juan ORtiz <jsortiz502@misena.edu.co>
2022-02-04 18:44:17 -0500

    se modifico vagabundo

--- vagabundo.py
+++ vagabundo.py
@@ -4,6 +4,14 @@ class Homeless:
     
 	def __init__(self, name):
 		self.name = name
+  		self.x = x
+    	self.y = y
+  
+	def posicion(self):
+		return(self.x, self.y)
+
+	def distance_origin(self):
+    	return(self.x**2 + self.y**2)**0.5
 
 class StandarHomeless(Homeless):
 	def __init__(self, name):
