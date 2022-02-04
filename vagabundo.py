cd81b736673b6809786075cdbe848cc6eab0cca8
Juan ORtiz <jsortiz502@misena.edu.co>
2022-02-04 18:54:58 -0500

    Creamos las clases de moderado y left_homeless

--- vagabundo.py
+++ vagabundo.py
@@ -1,16 +1,15 @@
 import random
 
 class Homeless:
+    def __init__(self, name):
+        self.name = name
+        self.x = x
+        self.y = y
+        
+    def posicion(self):
+        return(self.x, self.y)
     
-	def __init__(self, name):
-		self.name = name
-  		self.x = x
-    	self.y = y
-  
-	def posicion(self):
-		return(self.x, self.y)
-
-	def distance_origin(self):
+    def distance_origin(self):
     	return(self.x**2 + self.y**2)**0.5
 
 class StandarHomeless(Homeless):
@@ -18,5 +17,20 @@ class StandarHomeless(Homeless):
 		super().__init__(name)
 
 	def walk(self):
-		return random.choice([(0,1), (-1,0), (0,1), (0,-1)])
+		return random.choice([(0,2), (-2,0), (0,2), (0,-2)])
+
+class Left_Homeless(Homeless):
+    
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def walk(self):
+        dx, dy = random.choice([(0,1),(0,-5), (1,0), (-1,0)])
+        self.x += dx
+        self.y += dy
+        return(self.x, self.y)
+    
+    
+    
+    
 
